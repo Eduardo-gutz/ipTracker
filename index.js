ff6e10c5c160b2508f1d57dba390feca5f5bca85
Eduardo-gutz <eduardogutzfl@gmail.com>
2023-09-18 09:02:41 -0600

    add api key

---
+++ index.js
@@ -0,0 +1,4 @@
+const apiKey = 'at_26lHGdLzOPNEtc4KIxCJauLIHt9Se';
+
+// la url para la petición es : https://geo.ipify.org/api/v2/country?apiKey=apikey&ipAddress=8.8.8.8
+// recuerda que el ipAddress en la petición es dinámico, es decir, es el ip que el usuario ingrese en el input
